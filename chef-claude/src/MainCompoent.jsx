9e7f4169e0d980d7146499b70c4242fede8c0d7b
DileepaPRA <prabhath0115@gmail.com>
2026-03-22 16:41:27 +0530

    feat: implement dynamic ingredient list and form submission in MainComponent

--- chef-claude/src/MainCompoent.jsx
+++ chef-claude/src/MainCompoent.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
 
 function MainCompoent() {
-    const ingredient = ["Chicken", "Oregano", "Tomatoes"]
-    const ingredientListItems = ingredient.map(ingredient => {
-        return <li key={ingredient}>{ingredient}</li>
-    })
 
-    const submitHandler = () => {
-        console.log("form submitted");
+    const [ingredients, setIngredient] = useState([])
+
+    const ingredientListItems = ingredients.map(ingredient => {
+        return <li key={ingredient}>{ingredient}</li>
+    });
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredient((peviousIngrdient) => [...peviousIngrdient, newIngredient]);
+        
     }
 
   return (
-  <main className="w-[640px] border-x-2 border-b-2 border-gray-300 bg-stone-50 py-5 shadow-md">
-        <form className="flex items-center justify-center gap-10 py-[30px]">
+  <main className="w-[640px] border-x-2 border-b-2 border-gray-300 bg-stone-50 py-5 shadow-md flex flex-col">
+        <form className="flex items-center justify-center gap-10 py-[30px]" onSubmit={submitHandler}>
             <input 
                 aria-label="add ingredient"
                 placeholder="eg. oregano"
                 type="text"
-              className="rounded-md border border-gray-200 bg-white p-2 focus:border-gray-400 focus:outline-none"
+                name="ingredient"
+                className="rounded-md border border-gray-200 bg-white p-2 focus:border-gray-400 focus:outline-none"
             />
-            <button className="bg-black text-white py-2 px-6 rounded-lg shadow-xl" onSubmit={submitHandler}>+ Add ingredient</button>
+            <button className="bg-black text-white py-2 px-6 rounded-lg shadow-xl">+ Add ingredient</button>
         </form>
-        <ul>
-
+        <ul className="ml-30 list-disc">
+            {ingredientListItems}
         </ul>
     </main>
   )
