a9b29c40c9a8689fcf43fe7617ee80efb6ff64e0
DileepaPRA <prabhath0115@gmail.com>
2026-03-22 14:47:32 +0530

    feat: add ingredient list and submit handler to MainComponent

--- chef-claude/src/MainCompoent.jsx
+++ chef-claude/src/MainCompoent.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 
 function MainCompoent() {
+    const ingredient = ["Chicken", "Oregano", "Tomatoes"]
+    const ingredientListItems = ingredient.map(ingredient => {
+        return <li key={ingredient}>{ingredient}</li>
+    })
+
+    const submitHandler = () => {
+        console.log("form submitted");
+    }
+
   return (
   <main className="w-[640px] border-x-2 border-b-2 border-gray-300 bg-stone-50 py-5 shadow-md">
-    <form className="flex items-center justify-center gap-10 py-[30px]">
+        <form className="flex items-center justify-center gap-10 py-[30px]">
             <input 
                 aria-label="add ingredient"
                 placeholder="eg. oregano"
                 type="text"
               className="rounded-md border border-gray-200 bg-white p-2 focus:border-gray-400 focus:outline-none"
             />
-            <button className="bg-black text-white py-2 px-6 rounded-lg shadow-xl">+ Add ingredient</button>
+            <button className="bg-black text-white py-2 px-6 rounded-lg shadow-xl" onSubmit={submitHandler}>+ Add ingredient</button>
         </form>
+        <ul>
+
+        </ul>
     </main>
   )
 }
